85592f61e1bdf66c5c45e297f93bf8f0c5a83d83
ipcrm <mcadorette@puppetlabs.com>
2016-05-09 23:15:46 -0400

    updating limits

--- manifests/init.pp
+++ manifests/init.pp
@@ -11,6 +11,38 @@ class profile_wls (
   $src_dir   = '/var/tmp/',
   $log_dir   = '/var/tmp/',
 ){
+  sysctl { 'kernel.msgmnb':                 ensure => 'present', permanent => 'yes', value => '65536',}
+  sysctl { 'kernel.msgmax':                 ensure => 'present', permanent => 'yes', value => '65536',}
+  sysctl { 'kernel.shmmax':                 ensure => 'present', permanent => 'yes', value => '2147483648',}
+  sysctl { 'kernel.shmall':                 ensure => 'present', permanent => 'yes', value => '2097152',}
+  sysctl { 'fs.file-max':                   ensure => 'present', permanent => 'yes', value => '344030',}
+  sysctl { 'net.ipv4.tcp_keepalive_time':   ensure => 'present', permanent => 'yes', value => '1800',}
+  sysctl { 'net.ipv4.tcp_keepalive_intvl':  ensure => 'present', permanent => 'yes', value => '30',}
+  sysctl { 'net.ipv4.tcp_keepalive_probes': ensure => 'present', permanent => 'yes', value => '5',}
+  sysctl { 'net.ipv4.tcp_fin_timeout':      ensure => 'present', permanent => 'yes', value => '30',}
+  class { '::limits':
+    config => {
+      '*' => {
+        'nofile' => {
+          soft => '2048',
+          hard => '8192',
+        },
+      },
+      'oracle'  => {
+        'nofile' => {
+          soft => '65535',
+          hard => '65535',
+        },
+      'nproc'   => { soft => '2048'   , hard => '2048',   },
+      'memlock' => {
+        soft => '1048576',
+        hard => '1048576',
+        },
+      },
+    },
+    use_hiera => false,
+  }
+
   include ::orawls::urandomfix
   class{'::orawls::weblogic':
     version              => $version,
